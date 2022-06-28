303ba9360df2378f286bd2a79a9b8ea754889176
ebenadda <ebenadda@amazon.com>
2022-06-28 15:26:27 -0400

    Assigned numbers to cells so computer/player can select a specific cell.

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Board {
     public static void main(String[] args) {
         char[][] gameBoard = {
@@ -9,6 +11,42 @@ public class Board {
 
         };
         printGameBoard(gameBoard);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your placement (1 - 9)");
+        int position = scanner.nextInt();
+
+        System.out.println(position);
+
+        switch (position) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+        }
     }
 
     private static void printGameBoard(char[][] gameBoard) {
