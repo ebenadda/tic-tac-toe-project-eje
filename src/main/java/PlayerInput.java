ed1eefc504e734f809a628ebac317e2613daea53
jaurif <jauricflowers@gmail.com>
2022-06-28 08:58:07 -0700

    Stubbed out PlayerInput and game logic

---
+++ src/main/java/PlayerInput.java
@@ -0,0 +1,38 @@
+import java.security.KeyStore;
+import java.util.Scanner;
+
+public class PlayerInput {
+
+    //TODO Need a while statement for game status to determine if game is done or not.
+    // playerInput asks for player 'x' or 'o' if they are going
+    // then after the input we will reprint the tic tac toe game-board
+
+    //fields
+//    boolean playerX = true;
+//    boolean playerO;
+//
+
+//    while(finish.equals)("")){
+//    if(playerX){
+//        System.out.println("Player x turn");
+    // char[]
+//    }else{
+//        System.out.println("Player o turn");
+//    }
+
+    //TODO check to see if zone is occupied
+
+    //TODO plug input on the game
+    //getPlayerInput(playerx, gameboard);
+
+    /*
+    here we will need an if statement to check to see if the game is over and print the board
+    Also, need to check the board to see if cat game or not.
+     */
+//        if winner_winner(three_in_row){
+//            game_over = true;
+//            System.out.println("Winner Winner");
+//        }
+
+//    }
+}
