7709db0a1477989aaed33c088c6e6c013cfb50f4
ebenadda <ebenadda@amazon.com>
2022-06-29 17:33:05 -0400

    Revert "Updated WinConditions class"
    This reverts commit 4274d34311c81409a7bacf19f7228aa581726150.

--- src/main/java/WinConditions.java
+++ src/main/java/WinConditions.java
@@ -5,7 +5,6 @@ public class WinConditions {
 
     public static boolean isGameOver(char[][] gameBoard) {
 
-        //TOP ROW ACROSS
         if (gameBoard[0][0] == 'X'
                 && gameBoard[0][2] == 'X'
                 && gameBoard[0][4] == 'X') {
@@ -18,8 +17,18 @@ public class WinConditions {
             System.out.println("computer WINS!");
             return true;
         }
-
-        //  MIDDLE ROW ACROSS
+        if (gameBoard[1][0] == 'X'
+                && gameBoard[1][2] == 'X'
+                && gameBoard[1][4] == 'X') {
+            System.out.println("Player1 WINS!");
+            return true;
+        }
+        if (gameBoard[1][0] == 'O'
+                && gameBoard[1][2] == 'O'
+                && gameBoard[1][4] == 'O') {
+            System.out.println("Computer WINS!");
+            return true;
+        }
         if (gameBoard[2][0] == 'X'
                 && gameBoard[2][2] == 'X'
                 && gameBoard[2][4] == 'X') {
@@ -32,98 +41,75 @@ public class WinConditions {
             System.out.println("Computer WINS!");
             return true;
         }
-        // BOTTOM ROW ACROSS
-        if (gameBoard[4][0] == 'X'
-                && gameBoard[4][2] == 'X'
-                && gameBoard[4][4] == 'X') {
+        if (gameBoard[0][0] == 'X'
+                && gameBoard[1][0] == 'X'
+                && gameBoard[2][0] == 'X') {
             System.out.println("Player1 WINS!");
             return true;
         }
-        if (gameBoard[4][0] == 'O'
-                && gameBoard[4][2] == 'O'
-                && gameBoard[4][4] == 'O') {
+        if (gameBoard[0][0] == 'O'
+                && gameBoard[1][0] == 'O'
+                && gameBoard[2][0] == 'O') {
             System.out.println("Computer WINS!");
             return true;
         }
-
-        //FIRST COLUMN DOWN
-        if (gameBoard[0][0] == 'X'
-                && gameBoard[2][0] == 'X'
-                && gameBoard[4][0] == 'X') {
-            System.out.println("Player1 WINS");
-            return true;
-        }
-        if (gameBoard[0][0] == 'O'
-                && gameBoard[2][0] == 'O'
-                && gameBoard[4][0] == 'O') {
-            System.out.println("Computer WINS");
-            return true;
-        }
-
-        //MIDDLE COLUMN DOWN
         if (gameBoard[0][2] == 'X'
-                && gameBoard[2][2] == 'X'
-                && gameBoard[4][2] == 'X') {
+                && gameBoard[1][2] == 'X'
+                && gameBoard[2][2] == 'X') {
             System.out.println("Player1 WINS");
             return true;
         }
         if (gameBoard[0][2] == 'O'
-                && gameBoard[2][2] == 'O'
-                && gameBoard[4][2] == 'O') {
+                && gameBoard[1][2] == 'O'
+                && gameBoard[2][2] == 'O') {
             System.out.println("Computer WINS");
             return true;
         }
-
-        //RIGHT COLUMN DOWN
         if (gameBoard[0][4] == 'X'
-                && gameBoard[2][4] == 'X'
-                && gameBoard[4][4] == 'X') {
+                && gameBoard[1][4] == 'X'
+                && gameBoard[2][4] == 'X') {
             System.out.println("Player1 WINS");
             return true;
         }
         if (gameBoard[0][4] == 'O'
-                && gameBoard[2][4] == 'O'
-                && gameBoard[4][4] == 'O') {
+                && gameBoard[1][4] == 'O'
+                && gameBoard[2][4] == 'O') {
             System.out.println("Computer WINS");
             return true;
         }
-
-        // DIAG LEFT TO RIGHT
         if (gameBoard[0][0] == 'X'
-                && gameBoard[2][2] == 'X'
-                && gameBoard[4][4] == 'X') {
+                && gameBoard[1][2] == 'X'
+                && gameBoard[2][4] == 'X') {
             System.out.println("Player1 WINS");
             return true;
         }
         if (gameBoard[0][0] == 'O'
-                && gameBoard[2][2] == 'O'
-                && gameBoard[4][4] == 'O') {
+                && gameBoard[1][2] == 'O'
+                && gameBoard[2][4] == 'O') {
             System.out.println("Computer WINS");
             return true;
         }
-        // DIAG RIGHT TO LEFT
-        if (gameBoard[0][4] == 'X'
-                && gameBoard[2][2] == 'X'
-                && gameBoard[4][0] == 'X') {
-            System.out.println("Player1 WINS!");
+        if (gameBoard[2][0] == 'X'
+                && gameBoard[1][2] == 'X'
+                && gameBoard[0][4] == 'X') {
+            System.out.println("Player1 WINS");
             return true;
         }
-        if (gameBoard[0][4] == 'O'
-                && gameBoard[2][2] == 'O'
-                && gameBoard[4][0] == 'O') {
-            System.out.println("Computer WINS!");
+        if (gameBoard[2][0] == 'O'
+                && gameBoard[1][2] == 'O'
+                && gameBoard[0][4] == 'O') {
+            System.out.println("Computer WINS");
             return true;
         }
-
-        if (gameBoard[0][0] != ' '
-                && gameBoard[0][2] != ' '
-                && gameBoard[0][4] != ' '
+        if (gameBoard[0][0] != '_'
+                && gameBoard[0][2] != '_'
+                && gameBoard[0][4] != '_'
+                && gameBoard[1][0] != '_'
+                && gameBoard[1][2] != '_'
+                && gameBoard[1][4] != '_'
                 && gameBoard[2][0] != ' '
                 && gameBoard[2][2] != ' '
-                && gameBoard[2][4] != ' '
-                && gameBoard[4][0] != ' '
-                && gameBoard[4][2] != ' '
-                && gameBoard[4][4] != ' ') {
+                && gameBoard[0][4] != ' ') {
             System.out.println("CAT'S GAME!!");
         }
         return false;
